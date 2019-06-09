a27e1285bbf08327cd0c424680fa458c53026362
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-06-09 11:37:17 -0400

    first route established

---
+++ app/data/friends.js
@@ -0,0 +1,7 @@
+var friends = [
+  {
+    name: 'Paul',
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [4, 5, 6, 7, 8, 9, 1, 2, 3, 3]
+  }
+]
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const path = require('path');
 
+const route = require('./app/routing/htmlRoutes')
+
 var app = express();
-var PORT = 8000;
+var PORT = process.env.PORT || 8000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+route(app, PORT)
+
+app.listen(PORT, function() {
+  console.log('connected to port ' + PORT)
+})
