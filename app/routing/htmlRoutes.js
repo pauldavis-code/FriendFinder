a5da423a059680402a6451a922c3e8afcb3602ae
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-06-09 11:39:21 -0400

    added second html path

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,4 +4,7 @@ module.exports = function(app, port) {
   app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/home.html'));
   })
+  app.get('/survey', function(req, res) {
+    res.sendFile(path.join(__dirname, ('../public/survey.html')))
+  })
 }
