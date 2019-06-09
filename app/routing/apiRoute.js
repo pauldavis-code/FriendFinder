69095b7bcc0b93624bc6d8f9912b6486b7369f95
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-06-09 11:55:05 -0400

    added post route, tested w postman

--- app/routing/apiRoute.js
+++ app/routing/apiRoute.js
@@ -4,4 +4,10 @@ module.exports = function(app) {
   app.get('/api/friends', function(req, res) {
     return res.json(friends)
   })
+
+  app.post('/api/friends', function(req, res) {
+    let newFriend = req.body;
+
+    friends.push(newFriend)
+  })
 }
