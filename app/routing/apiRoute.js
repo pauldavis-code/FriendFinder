1cab627530d96e013a3ab9c341d2b80c679367a9
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-06-11 09:55:16 -0400

    added post functionality via form

--- app/routing/apiRoute.js
+++ app/routing/apiRoute.js
@@ -6,8 +6,8 @@ module.exports = function(app) {
   })
 
   app.post('/api/friends', function(req, res) {
+    console.log('sent')
     let newFriend = req.body;
-
     friends.push(newFriend)
   })
 }
